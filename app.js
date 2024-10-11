a316e8baf9057cf9ff775dca7fe75a9650814975
Al_P <al.e.poliakow@gmail.com>
2024-10-11 13:37:40 +1100

    Updated player icon for level 2

--- app.js
+++ app.js
@@ -534,17 +534,21 @@ levelTwo.addEventListener("click", function (e) {
 
     // create creature 
     const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    gridBot.setAttribute('fill', 'black'); //colours it in
-    gridBot.setAttribute('viewBox', '0 0 640 512'); //from svg link
+    gridBot.setAttribute('fill', '#916760'); //colours it in
+    gridBot.setAttribute('viewBox', '0 0 512 512'); //from svg link
     gridBot.setAttribute('stroke', '#5c5c5c'); // color
     gridBot.setAttribute("transform", `rotate(0)`); // to make advancing work before button pressing
-    iconPath.setAttribute("d", "M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z");
+    //iconPath.setAttribute("d", "M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z");
+    iconPath.setAttribute("d", "M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM135 241c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l87 87 87-87c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 345c-9.4 9.4-24.6 9.4-33.9 0L135 241z");
     iconPath.setAttribute('stroke-width', '6'); //thickness of lines
     gridBot.appendChild(iconPath);
     gridBot.classList.add("robot2"); // smaller size for smaller square
     //append creature to first div
     gameBoard.firstChild.append(gridBot);
 
+
+    //append creature to first div
+    gameBoard.firstChild.append(gridBot);
     // create a button to rotate the robot upwards
     const rotateUp = document.createElement("button");
     rotateUp.innerText = "Face up";
@@ -670,7 +674,7 @@ levelTwo.addEventListener("click", function (e) {
                             div.appendChild(gridBot);
                             console.log("attempted to shift left");
                         }
-                    } else if (parent == 1 | parent == 11 | parent == 21 | parent == 41 | parent == 71 | parent == 81 | parent == 91) {
+                    } else if (parent == 1 | parent == 11 | parent == 21 | parent == 31 |parent == 41 | parent == 71 | parent == 81 | parent == 91) {
                         instructions.innerText = "You've hit a wall";
                     } else {
                         instructions.innerText = "You've hit a wall";

9e3c6e56aadfb5108f6d278601e61bd51ca17c9f
Al_P <al.e.poliakow@gmail.com>
2024-11-01 15:44:44 +1100

    Contrast checked and corrected

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ demoLevel.addEventListener("click", function (e) {
     const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     gridBot.setAttribute('fill', '#916760'); //colours it in
     gridBot.setAttribute('viewBox', '0 0 512 512'); //from svg link
-    gridBot.setAttribute('stroke', '#5c5c5c'); // color
+    gridBot.setAttribute('stroke', '#916760'); // color
     gridBot.setAttribute("transform", `rotate(0)`); // to make advancing work before button pressing
     //iconPath.setAttribute("d", "M320 0c17.7 0 32 14.3 32 32l0 64 120 0c39.8 0 72 32.2 72 72l0 272c0 39.8-32.2 72-72 72l-304 0c-39.8 0-72-32.2-72-72l0-272c0-39.8 32.2-72 72-72l120 0 0-64c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16l32 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-32 0zM264 256a40 40 0 1 0 -80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224l16 0 0 192-16 0c-26.5 0-48-21.5-48-48l0-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48l0 96c0 26.5-21.5 48-48 48l-16 0 0-192 16 0z");
     iconPath.setAttribute("d", "M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM135 241c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l87 87 87-87c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 345c-9.4 9.4-24.6 9.4-33.9 0L135 241z");
